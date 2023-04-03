74a0c17d008a85723c4caca752bf7b8f098421d6
Mussued <skossm@outlook.com>
2023-04-03 21:21:16 +0800

    refactor: 使用AntD重构Layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,27 @@
 import React, { FC } from 'react'
 import { Outlet } from 'react-router-dom'
+import { Layout } from 'antd'
+import Logo from '../components/Logo'
+import styles from './MainLayout.module.scss'
+
+const { Header, Content, Footer } = Layout
 
 const MainLayout: FC = () => {
   return (
-    <>
-      <div>MainLayout header</div>
-      <div>
-        <Outlet />
-      </div>
-      <div>MainLayout footer</div>
-    </>
+    <Layout>
+      <Header className={styles.header}>
+        <div className={styles.left}>
+          <Logo />
+        </div>
+        <div className={styles.right}>登录/用户信息</div>
+      </Header>
+      <Layout className={styles.main}>
+        <Content>
+          <Outlet />
+        </Content>
+      </Layout>
+      <Footer className={styles.footer}>问卷星 &copy;2023 - present. Created by Mussued</Footer>
+    </Layout>
   )
 }
 
