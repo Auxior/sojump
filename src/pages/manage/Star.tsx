f8018cf78fe17d0fb6948e5022b7a2651cfa6639
Mussued <skossm@outlook.com>
2023-04-04 14:59:26 +0800

    feat: 让ListSearch搜索修改URL参数

--- src/pages/manage/Star.tsx
+++ src/pages/manage/Star.tsx
@@ -2,6 +2,7 @@ import React, { FC, useState } from 'react'
 import { useTitle } from 'ahooks'
 import { Typography, Empty } from 'antd'
 import QuestionCard from '../../components/QuestionCard'
+import ListSearch from '../../components/ListSearch'
 import styles from './common.module.scss'
 
 const { Title } = Typography
@@ -44,14 +45,15 @@ const Star: FC = () => {
         <div className={styles.left}>
           <Title level={3}>星标问卷</Title>
         </div>
-        <div className={styles.right}> (搜索) </div>
+        <div className={styles.right}>
+          <ListSearch />
+        </div>
       </div>
       <div className={styles.content}>
         {questionList.length === 0 && <Empty description="暂无数据" />}
         {questionList.length > 0 &&
           questionList.map(q => {
             const { _id } = q
-
             return <QuestionCard key={_id} {...q} />
           })}
       </div>
